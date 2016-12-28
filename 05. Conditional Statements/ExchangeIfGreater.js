8db1f2b1cc05618d383492e00f75c290551d006f
debss <dzhelyazkova@abv.bg>
2016-12-28 14:45:08 +0200

    Ex. 1 from Homework

---
+++ 05. Conditional Statements/ExchangeIfGreater.js
@@ -0,0 +1,16 @@
+function solve(args) {
+    var a = +args[0];
+    var b = +args[1];
+
+if(a>b) {
+    console.log(b, a);
+}
+else if(b>a) {
+    console.log(a, b);
+}
+else{
+    console.log(a, b);
+}
+}
+
+//solve(['4','3']);
