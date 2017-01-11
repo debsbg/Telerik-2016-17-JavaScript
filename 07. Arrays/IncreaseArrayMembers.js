843b185230f32f70c707c7643fbf36ac3daa55f5
debsbg <debora_zhelyazkova@abv.bg>
2017-01-11 15:14:19 +0200

    Icrease Array Members

---
+++ 07. Arrays/IncreaseArrayMembers.js
@@ -0,0 +1,12 @@
+function solve(args) {
+    var n = +args[0];
+    var arr = [];
+    var i, len;
+    arr.length = n;
+   // var arr1 = arr[n];
+for (i = 0, len = 5*n; i < len; i+=5) {
+    console.log(i);
+    //i +=5;
+}
+
+}
