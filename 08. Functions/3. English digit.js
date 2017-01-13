ba02376e71ea3f64baa6383d80fa9989257fd886
debsbg <debora_zhelyazkova@abv.bg>
2017-01-13 10:06:54 +0200

    EnglishDigit

---
+++ 08. Functions/3. English digit.js
@@ -0,0 +1,30 @@
+function solve(args){
+    let str = args[0];
+    let n = str % 10;
+
+result(n);
+
+function result(n){
+    switch (n) {
+    case 0: console.log('zero'); break;
+    case 1: console.log('one'); break;
+    case 2: console.log('two'); break;
+    case 3: console.log('three'); break;
+    case 4: console.log('four'); break;
+    case 5: console.log('five'); break;
+    case 6: console.log('six'); break;
+    case 7: console.log('seven'); break;
+    case 8: console.log('eight'); break;
+    case 9: console.log('nine'); break;
+
+
+    default: console.log('Error');
+        break;
+}
+}
+
+
+}
+
+//solve(['42']);
+//two
