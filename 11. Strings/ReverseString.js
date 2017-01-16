6d2e765e82ac80d29059bac9867a65596cf484a3
debsbg <debora_zhelyazkova@abv.bg>
2017-01-16 11:20:47 +0200

    homework

---
+++ 11. Strings/ReverseString.js
@@ -0,0 +1,10 @@
+function Solve(args) {
+    let str = args[0].split(""),
+        revStr = str.reverse(); 
+        joinArr = revStr.join("");
+
+    console.log(joinArr);
+}
+
+//Solve(['sample']);
+// elpmas
